f1d95d580066d322000c941cacd02586ff3dddf7
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-01-24 17:39:36 +0100

    Add modify html + js + appearance

--- js/main.js
+++ js/main.js
@@ -17,3 +17,50 @@
 // Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l'utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 // Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
 
+
+    const Button = document.getElementById('ButtonGenera');
+
+    Button.addEventListener(
+        'click',
+
+    function () {
+
+        //const & if
+        const User = document.getElementById('nome').value;
+        const Km = parseInt(document.getElementById('km').value);
+        const Anni = document.getElementById('age').value;
+        const DefaultPrice = Km * 0.21;
+        let Price;
+
+        if (!isNaN(Km)) {
+
+        } else {
+            alert('RIPROVARE: inserire valore numerico')
+        }
+
+
+        if (Anni < 18) {
+
+            Price = (DefaultPrice - (20 / 100) * (DefaultPrice)); 
+
+        } else if (Anni > 64) {
+
+            Price = (DefaultPrice - (40 / 100) * (DefaultPrice));
+
+        } else {
+
+             Price = DefaultPrice;
+
+        }
+
+       
+       //toFixed
+        Price = Price.toFixed(2);
+
+       //Output
+        document.getElementById('h2').innerHTML = `Il prezzo totale per il tuo viaggio è di ${Price} €`;
+
+    }
+)
+
+
